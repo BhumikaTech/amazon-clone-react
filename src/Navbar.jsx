9a83085c5df4a8b62b9ccb58ca7b8a445d6af419
Bhumika <bhumikabhatia123456@gmail.com>
2026-08-29 13:01:24 +0530

    Add and adjust Amazon logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,20 +2,21 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar({ cartItems = [], search, setSearch }) {
-
   const handleSearch = () => {
     alert(`Searching for: ${search}`);
   };
-
 
   return (
     <div className="navbar">
 
       {/* Logo */}
       <div className="nav-logo">
-        <div className="logo"></div>
+        <img
+          src="/amazon_logo.png"
+          alt="Amazon"
+          className="logo"
+        />
       </div>
-
 
       {/* Address */}
       <div className="nav-address">
@@ -25,17 +26,13 @@ function Navbar({ cartItems = [], search, setSearch }) {
           <i className="fa-solid fa-location-dot"></i>
           <p className="add-second">India</p>
         </div>
-
       </div>
-
 
       {/* Search */}
       <div className="nav-search">
-
         <select className="search-select">
           <option>All</option>
         </select>
-
 
         <input
           type="text"
@@ -45,63 +42,48 @@ function Navbar({ cartItems = [], search, setSearch }) {
           onChange={(e) => setSearch(e.target.value)}
         />
 
-
         <div className="search-icon">
           <i className="fa-solid fa-magnifying-glass"></i>
         </div>
-
       </div>
-
-
 
       {/* Language */}
       <div className="nav-lan">
-
-        <img 
-          src="/images/us_flag.png" 
-          width="25" 
-          alt="US Flag" 
+        <img
+          src="/images/us_flag.png"
+          width="25"
+          alt="US Flag"
         />
 
         <p>EN</p>
 
-        <img 
-          src="/images/dropdown_icon.png" 
-          width="8" 
-          alt="Dropdown" 
+        <img
+          src="/images/dropdown_icon.png"
+          width="8"
+          alt="Dropdown"
         />
-
       </div>
-
-
-
 
       {/* Account */}
       <div className="nav-text">
-
-       <p>
-        <Link to="/signin">
-        Hello, Sign in
-       </Link>
-       </p>
+        <p>
+          <Link to="/signin">
+            Hello, Sign in
+          </Link>
+        </p>
 
         <h1>
           Account & Lists{" "}
-          <img 
-            src="/images/dropdown_icon.png" 
-            width="8" 
-            alt="Dropdown" 
+          <img
+            src="/images/dropdown_icon.png"
+            width="8"
+            alt="Dropdown"
           />
         </h1>
-
       </div>
-
-
-
 
       {/* Orders */}
       <div className="nav-text">
-
         <p>
           Returns
         </p>
@@ -109,11 +91,7 @@ function Navbar({ cartItems = [], search, setSearch }) {
         <h1>
           & Orders
         </h1>
-
       </div>
-
-
-
 
       {/* Mobile User */}
       <Link
@@ -121,36 +99,27 @@ function Navbar({ cartItems = [], search, setSearch }) {
         className="mobile-user-icon"
         style={{ display: "none" }}
       >
-
-        <img 
-          src="/images/user.png" 
-          alt="User" 
+        <img
+          src="/images/user.png"
+          alt="User"
         />
-
       </Link>
 
-
-
-
       {/* Cart */}
-      <Link 
-        to="/cart" 
+      <Link
+        to="/cart"
         className="nav-cart"
       >
-
-        <img 
-          src="/images/cart_icon.png" 
-          width="35" 
-          alt="Cart" 
+        <img
+          src="/images/cart_icon.png"
+          width="35"
+          alt="Cart"
         />
-
 
         <h4>
           Cart {cartItems.length}
         </h4>
-
       </Link>
-
 
     </div>
   );
